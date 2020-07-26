9767953b077fb480bdd0c0769863b2d9362a9e08
Chris <cciric44@gmail.com>
2020-07-25 20:12:48 -0400

    created other pages in navbar to navigate to

---
+++ script.js
@@ -0,0 +1,31 @@
+const element = document.getElementById('container');
+
+function navigateTo(e){
+    let element = e.target.innerHTML;
+
+    switch (element) {
+        case "Products":
+        window.location.replace("products.html");
+        break;
+        case "FAQ":
+        window.location.replace("faq.html");
+        break;
+        case "Location":
+        window.location.replace("location.html");
+        break;
+        case "Home":
+        window.location.replace("index.html")
+        break;
+
+    }
+
+    //this can work as well, but back button issues?
+    // window.location.href = 'products.html';
+
+   
+};
+
+element.addEventListener("click", navigateTo, false);
+
+
+
